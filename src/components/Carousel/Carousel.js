15436495f37a4a75db8f4a3413530dbd868e6770
jaswanth <jaswanthbeere@gmail.com>
2022-09-01 14:07:09 +0530

    changed sponsors images

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,9 +1,9 @@
 "use strict";
 import { forwardRef } from "react";
 import "./Carousel.css";
-import tesla from "../../assets/tesla.png";
-import spacex from "../../assets/spacex.png";
-import netflix from "../../assets/netflix.png";
+import tesla from "../../assets/Budding_Analyst.png";
+import spacex from "../../assets/seedup.png";
+import netflix from "../../assets/internshala.jpg";
 
 const Carousel = forwardRef((props, ref) => {
   return (
@@ -15,13 +15,38 @@ const Carousel = forwardRef((props, ref) => {
         <input type="radio" name="slider" id="item-3" />
         <div className="cards">
           <label className="sponsors-card" htmlFor="item-1" id="sponsor-card-1">
-            <img className="image" src={spacex} alt="song" />
+            <img
+              className="image"
+              style={{
+                backgroundColor: "white",
+                borderRadius: "3px",
+                padding: "2rem",
+              }}
+              src={spacex}
+              alt="song"
+            />
           </label>
           <label className="sponsors-card" htmlFor="item-2" id="sponsor-card-2">
-            <img className="image" src={tesla} alt="song" />
+            <img
+              className="image"
+              style={{
+                backgroundColor: "white",
+                borderRadius: "3px",
+              }}
+              src={tesla}
+              alt="song"
+            />
           </label>
           <label className="sponsors-card" htmlFor="item-3" id="sponsor-card-3">
-            <img className="image" src={netflix} alt="song" />
+            <img
+              className="image"
+              style={{
+                backgroundColor: "white",
+                borderRadius: "3px",
+              }}
+              src={netflix}
+              alt="song"
+            />
           </label>
         </div>
       </div>
